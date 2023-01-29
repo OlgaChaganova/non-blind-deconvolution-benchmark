734c972771235dc9b91ac6d9906a4ed5cc36b160
OlgaChaganova <olga.chaganova1@gmail.com>
2023-01-29 13:58:10 +0300

    Add imsshow for several images showing

--- src/data/utils.py
+++ src/data/utils.py
@@ -13,9 +13,21 @@ def read_image(img_path: str) -> np.array:
     return (image / 255).astype(np.float32)
 
 
-
-def imshow(image: np.array, figsize: tp.Tuple[int, int]):
+def imshow(image: np.array, figsize: tp.Tuple[int, int], title: tp.Optional[str] = None):
     plt.figure(figsize=figsize)
     plt.imshow(image)
     plt.axis('off')
+    if title is not None:
+        plt.title(title)
     plt.show()
+
+
+def imsshow(images: tp.List[np.array], figsize: tp.Tuple[int, int], titles: tp.Optional[tp.List[str]] = None):
+    """Show several images."""
+    _, axs = plt.subplots(1, len(images), figsize=figsize)
+    for i, image in enumerate(images):
+        axs[i].imshow(image)
+        axs[i].axis('off')
+        if titles is not None:
+            axs[i].set_title(titles[i])
+
