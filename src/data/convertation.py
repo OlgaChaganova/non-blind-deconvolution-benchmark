03044c743a900393eaf353be433aa08a3504f2e1
OlgaChaganova <olga.chaganova1@gmail.com>
2023-04-05 22:37:42 +0300

    Add convertation

--- src/data/convertation.py
+++ src/data/convertation.py
@@ -1,7 +1,9 @@
 import numpy as np
+import numpy.typing as npt
 
+from constants import MAX_UINT8, MAX_UINT16
 
-def srgbf_to_linrgbf(image: np.array) -> np.array:
+def srgbf_to_linrgbf(image: npt.NDArray[np.float_]) -> npt.NDArray[np.float32]:
     """Conver image from float sRGB to float linRGB"""
     assert image.dtype in [np.float16, np.float32, np.float64], f'Image dtype must be np.float(16/32/64), but got {image.dtype}'
     assert image.max() <= 1, f'Image max must be <= 1, but got {image.max()}'
@@ -9,33 +11,34 @@ def srgbf_to_linrgbf(image: np.array) -> np.array:
     mask = image <= 0.04045
     image[mask] = image[mask] / 12.92
     image[np.invert(mask)] = ((image[np.invert(mask)] + 0.055) / 1.055) ** 2.4
-    return image
+    return image.astype(np.float32)
 
 
-def float_to_uint16(image: np.array) -> np.array:
+def float_to_uint16(image: npt.NDArray[np.float_]) -> npt.NDArray[np.uint16]:
     """Convert image from float (lin / sRGB) to 16 bit (lin / sRGB)"""
     assert image.dtype in [np.float16, np.float32, np.float64], f'Image dtype must be np.float(16/32/64), but got {image.dtype}'
     assert image.max() <= 1, f'Image max must be <= 1, but got {image.max()}'
     assert image.min() >= 0, f'Image min must be >= 0, but got {image.min()}'
-    max_uint16 = 65535
-    return (image * max_uint16).astype(np.uint16)
+    return (image * MAX_UINT16).astype(np.uint16)
 
 
-def linrrgb16_to_srgb8(image: np.array) -> np.array:
+def linrrgb16_to_srgb8(image: npt.NDArray[np.uint16]) -> npt.NDArray[np.uint8]:
     """Convert image from 16 bit Linear RGB 16 bit to SRGB 8 bit """
     assert image.dtype == np.uint16, f'Image dtype must be np.uint16, but got {image.dtype}'
-    max_uint16 = 65535
-    max_uint8 = 255
-    image = image / max_uint16
+    image = image / MAX_UINT16
     mask = image <= 0.0031308
     image[mask] = image[mask] * 12.92
     image[np.invert(mask)] = 1.055 * (image[np.invert(mask)] ** (1/2.4)) - 0.055
-    return (image  * max_uint8).astype(np.uint8)
+    return (image  * MAX_UINT8).astype(np.uint8)
 
 
-def uint16_to_uint8(image: np.array) -> np.array:
+def uint16_to_uint8(image: npt.NDArray[np.uint16]) -> npt.NDArray[np.uint8]:
     """Convert image from 16 bit (lin / sRGB) to 8 bit (lin / sRGB)"""
     assert image.dtype == np.uint16, f'Image dtype must be np.uint16, but got {image.dtype}'
-    max_uint16 = 65535
-    max_uint8 = 255
-    return ((image / max_uint16) * max_uint8).astype(np.uint8)
+    return ((image / MAX_UINT16) * MAX_UINT8).astype(np.uint8)
+
+
+
+def uint8_to_float32(image: npt.NDArray[np.uint8]) -> npt.NDArray[np.float32]:
+    """Convert image from 8 bit (lin / sRGB) to float32 (lin / sRGB)"""
+    return (image / MAX_UINT8).astype(np.float32)
