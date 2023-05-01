eca73b5b916bed3efd6f533b95ed6b24c77036d9
OlgaChaganova <olga.chaganova1@gmail.com>
2023-05-01 21:12:33 +0300

    Add experiment 1

--- src/measure_blur_strength.py
+++ src/measure_blur_strength.py
@@ -6,10 +6,9 @@ from copy import deepcopy
 import numpy as np
 from tqdm import tqdm
 
-from constants import IMAGE_SIZE
-from data.convertation import srgbf_to_linrgbf
+from data.convertation import srgbf_to_linrgbf, uint8_to_float32
 from data.convolution import convolve
-from imutils import center_crop, imread, load_npy, srgb2gray
+from imutils import imread, load_npy, srgb2gray
 from metrics import psnr, ssim
 
 
@@ -22,7 +21,7 @@ def calc_stats(values: np.array) -> tp.Tuple[float, float, float, float]:
     return mean_value, std_value, max_value, min_value
 
 
-def main(benchmark_list_path: str):
+def main(benchmark_list_path: str) -> tp.Dict[str, tp.List[float]]:
     psnr_ssim_dict = {'psnr': [], 'ssim': []}
     metrics = dict()
     for blur_type in ['gauss_blur', 'motion_blur', 'small_eye_blur', 'medium_eye_blur', 'big_eye_blur']:
@@ -32,27 +31,31 @@ def main(benchmark_list_path: str):
         next(file)
         for line in tqdm(file):
             blur_type, _, kernel_path, _, image_path = line.strip().split(',')
-            if image_path.endswith('.png'):
-                if blur_type == 'eye_blur':
-                    if 'small-psf' in kernel_path:
-                        blur_type = 'small_' + blur_type
-                    elif 'medium-psf' in kernel_path:
-                        blur_type = 'medium_' + blur_type
-                    elif 'big-psf' in kernel_path:
-                        blur_type = 'big_' + blur_type
 
-                image = imread(image_path)
-                image = center_crop(image, IMAGE_SIZE, IMAGE_SIZE)
-                if image.ndim == 3:
-                    image = srgb2gray(image)
-                image = srgbf_to_linrgbf(image)  # convert from float sRGB to linRGB
+            if blur_type == 'eye_blur':
+                if 'small-psf' in kernel_path:
+                    blur_type = 'small_' + blur_type
+                elif 'medium-psf' in kernel_path:
+                    blur_type = 'medium_' + blur_type
+                elif 'big-psf' in kernel_path:
+                    blur_type = 'big_' + blur_type
 
-                kernel = load_npy(kernel_path, key='psf')
+            image = imread(image_path)
 
-                blurred = convolve(image, kernel).astype(np.float32)
+            if image_path.endswith('.jpg'):  # sRGB 8 bit
+                image = uint8_to_float32(image)  # sRGB float
 
-                metrics[blur_type]['psnr'].append(psnr(image, blurred))
-                metrics[blur_type]['ssim'].append(ssim(image, blurred))
+            if image.ndim == 3:
+                image = srgb2gray(image)  # sRGB float
+            
+            image = srgbf_to_linrgbf(image)  # linRGB float
+
+            kernel = load_npy(kernel_path, key='psf')
+
+            blurred = convolve(image, kernel).astype(np.float32)
+
+            metrics[blur_type]['psnr'].append(psnr(image, blurred))
+            metrics[blur_type]['ssim'].append(ssim(image, blurred))
 
     logging.info('Evaluating blur strength for different blur types:')
     for blur_type in metrics.keys():
@@ -62,6 +65,7 @@ def main(benchmark_list_path: str):
             f'For {blur_type}: PSNR: max: {max_psnr:.3f}, min: {min_psnr:.3f}, mean: {mean_psnr:.3f}, std: {std_psnr:.3f}; '
             f'SSIM: max: {max_ssim:.3f},  min: {min_ssim:.3f}, mean: {mean_ssim:.3f}, std: {std_ssim:.3f}',
         )
+    return metrics
 
 
 if __name__ == '__main__':
